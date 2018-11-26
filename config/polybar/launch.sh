1f448bd5c36a4a79c31f62d966de245c6a5983fb
E.Azer Koçulu <azer@kodfabrik.com>
2018-11-26 08:29:36 +0800

    Initial commit

---
+++ config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#polybar example &
+
+polybar work
+
+#for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
+#feh --bg-scale ~/wallpaper.jpg
+
+#echo "Bars launched..."
